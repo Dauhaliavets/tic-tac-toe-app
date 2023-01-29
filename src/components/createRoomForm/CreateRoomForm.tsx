73806b950900486fbcdddf0e878c6a63b2daf761
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-29 15:19:26 +0300

    feat: add createRoomForm

---
+++ src/components/createRoomForm/CreateRoomForm.tsx
@@ -0,0 +1,35 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import { Socket } from 'socket.io-client'
+
+export const CreateRoomForm = ({ socket }: { socket: Socket }) => {
+  const [roomName, setRoomName] = useState<string>('')
+  const navigate = useNavigate()
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setRoomName(event.target.value)
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    socket.emit('createRoom', { roomName })
+    navigate(`/home/room/${roomName}`)
+  }
+
+  return (
+    <Form className='w-75 d-flex justify-content-around align-items-center' onSubmit={handleSubmit}>
+      <Form.Group className='w-75'>
+        <Form.Control
+          type='text'
+          value={roomName}
+          onChange={handleChange}
+          placeholder='Enter room name'
+        />
+      </Form.Group>
+      <Button variant='primary' type='submit' disabled={!roomName}>
+        Create room
+      </Button>
+    </Form>
+  )
+}
