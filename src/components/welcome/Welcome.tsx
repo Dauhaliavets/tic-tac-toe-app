2e78419227da762daa47a0e69b72378b07be9568
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-29 18:05:30 +0300

    refactor: minor refactor

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, useContext, useEffect, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useContext, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { Socket } from 'socket.io-client'
